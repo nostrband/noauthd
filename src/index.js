67d5ccff80d9274c0ff2735db10543130ed47b62
artur <brugeman.artur@gmail.com>
2024-02-09 15:37:50 +0300

    Use x-real-ip for pow rate limiter

--- src/index.js
+++ src/index.js
@@ -457,7 +457,7 @@ function getMinPow(name, req) {
     minPow += 4;
   }
 
-  const ip = req.ip
+  const ip = req.header('x-real-ip')?.[0] || req.ip
 
   // have a record for this ip?
   const { pow: lastPow = 0, tm = 0 } = ipNamePows.get(ip) || {};
